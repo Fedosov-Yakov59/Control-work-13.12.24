77901baad72dee94843e6478c4a85ecac7b45f6b
Fedosov-Yakov59 <yakov.fed6@gmail.com>
2024-12-13 11:48:01 +0700

    🎉 Погрузитесь в мир увлекательной игры "Камень, Ножницы, Бумага"! 🎉
    Вы готовы проверить свои навыки против компьютера? С помощью этого простого, но захватывающего кода на Python вы сможете сыграть в классическую игру "Камень, Ножницы, Бумага" прямо у себя на компьютере!
    
    Что делает этот код?
    Выбор игрока: Вы можете выбрать один из трех вариантов: камень, бумага или ножницы. Просто введите соответствующее число (0, 1 или 2), и ваш выбор будет обработан!
    
    Случайный выбор компьютера: Компьютер также делает свой выбор случайным образом, что добавляет элемент неожиданности в игру.
    
    Определение победителя: Код сравнивает ваши действия с действиями компьютера и объявляет победителя, а также сообщает о ничьей.
    
    Повторная игра: После каждой игры вам будет предложено сыграть еще раз. Просто введите "д" для продолжения или "н" для выхода.
    
    Преимущества:
    Легкость в использовании: Игра проста в освоении и не требует сложных настроек.
    Развлечение для всех: Подходит для игроков всех возрастов.
    Отличный способ практиковать Python: Если вы изучаете программирование, этот код — отличный пример для практики.
    Как запустить:
    Скопируйте код в ваш Python-редактор.
    Запустите программу и следуйте инструкциям на экране.
    Наслаждайтесь игрой и проверьте, сможете ли вы обыграть компьютер!
    Не упустите возможность развлечься и улучшить свои навыки! 🥳💻

--- Контрольная работа 13.12.24.py
+++ Контрольная работа 13.12.24.py
@@ -1,35 +1,61 @@
-num1 = int(input("Введите первое число: "))
-num2 = int(input("Введите второе число: "))
+import random
+from enum import IntEnum
 
-print("Первое число:", num1)
-print("Второе число:",  num2)
+class Action(IntEnum):
+    Камень = 0
+    Бумага = 1
+    Ножницы = 2
 
-message = '''
-Выберете математическую операцию:
+    def get_user_selection():
+        user_input = input("Сделайте выбор — (камень[0], бумага[1], ножницы[2]): ")
+        selection = int(user_input)
+        action = Action(selection)
+        return action
+        def get_user_selection():
+    choices = [f"{action.name}[{action.value}]" for action in Action]
+    choices_str = ", ".join(choices)
+    selection = int(input(f"Сделайте выбор — ({choices_str}): "))
+    action = Action(selection)
+    return action
+def get_user_selection():
+    choices = [f"{action.name}[{action.value}]" for action in Action]
+    choices_str = ", ".join(choices)
+    selection = int(input(f"Сделайте выбор — ({choices_str}): "))
+    action = Action(selection)
+    return action
+def get_computer_selection():
+    selection = random.randint(0, len(Action) - 1)
+    action = Action(selection)
+    return action
+def determine_winner(user_action, computer_action):
+    if user_action == computer_action:
+        print(f"Оба пользователя выбрали {user_action.name}. Ничья!")
+    elif user_action == Action.Камень:
+        if computer_action == Action.Ножницы:
+            print("Камень бьет ножницы! Вы победили!")
+        else:
+            print("Бумага оборачивает камень! Вы проиграли.")
+    elif user_action == Action.Бумага:
+        if computer_action == Action.Камень:
+            print("Бумага оборачивает камень! Вы победили!")
+        else:
+            print("Ножницы режут бумагу! Вы проиграли.")
+    elif user_action == Action.Ножницы:
+        if computer_action == Action.Бумага:
+            print("Ножницы режут бумагу! Вы победили!")
+        else:
+            print("Камень бьет ножницы! Вы проиграли.")
+            while True:
+                try:
+                    user_action = get_user_selection()
+                except ValueError as e:
+                    range_str = f"[0, {len(Action) - 1}]"
+                    print(f"Некорректный ввод. Введите значение из промежутка {range_str}")
+                    continue
 
-+ : Сложение
-- : Вычитание
-/ : Деление
-* : Умножение
+                computer_action = get_computer_selection()
+                determine_winner(user_action, computer_action)
 
-Ваш выбор:
-'''
-operation = input(message)
-
-if operation == '+':
-    print('Сложение')
-    result = num1 + num2
-elif operation == '-':
-    print('Вычитание')
-    result = num1 - num2
-elif operation == '/':
-    print('Деление')
-    result = num1 / num2
-elif operation == '*':
-    print('Умножение')
-    result = num1 * num2
-else:
-    print('Неизвестная операция')
-
-
-print("Результат:", result)
+                play_again = input("Сыграем еще? (д/н): ")
+                if play_again.lower() != "д":
+                    break
